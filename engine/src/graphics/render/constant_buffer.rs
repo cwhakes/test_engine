32d62951282ced63090fa699259ad12da7f935c0
Will Hakes <info@cwilliamhakes.com>
2020-05-04 01:38:11 -0600

    Fix `ConstantWrapper` not being used everywhere

--- engine/src/graphics/render/constant_buffer.rs
+++ engine/src/graphics/render/constant_buffer.rs
@@ -33,7 +33,7 @@ impl<C> ConstantBuffer<C> {
         unsafe {
             let mut buff_desc = d3d11::D3D11_BUFFER_DESC::default();
             buff_desc.Usage = d3d11::D3D11_USAGE_DEFAULT;
-            buff_desc.ByteWidth = (std::mem::size_of::<C>()) as u32;
+            buff_desc.ByteWidth = (std::mem::size_of::<ConstantWrapper<C>>()) as u32;
             buff_desc.BindFlags = d3d11::D3D11_BIND_CONSTANT_BUFFER;
             buff_desc.CPUAccessFlags = 0;
             buff_desc.MiscFlags = 0;
@@ -58,13 +58,13 @@ impl<C> ConstantBuffer<C> {
         self.buffer.as_ptr()
     }
 
-    pub fn update(&mut self, context: &Context, mut buffer: C) {
+    pub fn update(&mut self, context: &Context, buffer: C) {
         unsafe {
             context.as_ref().UpdateSubresource(
                 &**self.buffer.as_ref() as *const _ as *mut _,
                 0,
                 ptr::null_mut(),
-                (&mut buffer) as *mut _ as *mut _,
+                (&mut ConstantWrapper(buffer)) as *mut _ as *mut _,
                 0,
                 0,
             );
