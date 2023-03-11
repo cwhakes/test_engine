6d46cf00dbf6c039da1fd305523469798c449ac9
Will Hakes <info@cwilliamhakes.com>
2023-03-10 22:22:23 -0700

    Add rref for matricies

--- libs/engine/src/math/matrix.rs
+++ libs/engine/src/math/matrix.rs
@@ -3,6 +3,8 @@ use super::Vector;
 use std::mem::{self, MaybeUninit};
 use std::{iter, ops};
 
+use float_cmp::approx_eq;
+
 #[repr(C)]
 #[derive(Clone, Debug, PartialEq)]
 pub struct Matrix<T, const M: usize, const N: usize>(pub [[T; N]; M]);
@@ -23,9 +25,67 @@ impl<T, const M: usize, const N: usize> Matrix<MaybeUninit<T>, M, N> {
     }
 }
 
+impl<T, const M: usize, const N: usize> Matrix<T, M, N> {
+    pub fn transverse(self) -> Matrix<T, N, M> {
+        // SAFETY: All elements are written to
+        unsafe {
+            let mut new = Matrix::uninit();
+            for (i, row) in self.0.into_iter().enumerate() {
+                for (j, ele) in row.into_iter().enumerate() {
+                    new[(j, i)].write(ele);
+                }
+            }
+            new.assume_init()
+        }
+    }
+}
+
 impl<const M: usize, const N: usize> Matrix<f32, M, N> {
     pub fn zero() -> Self {
         Self([[0.0; N]; M])
+    }
+
+    pub fn rref(mut self) -> Self {
+        let (mut i, mut j) = (0, 0);
+        'outer: while i < M && j < N {
+            // Swap rows if pivot is zero
+            if approx_eq!(f32, 0.0, self[(i, j)]) {
+                for i_p in (i + 1)..M {
+                    if !approx_eq!(f32, 0.0, self[(i_p, j)]) {
+                        self.0.swap(i, i_p);
+                        break;
+                    }
+                }
+                // If we can't find a non-zero element in this column, advance pivot column
+                j += 1;
+                continue 'outer;
+            }
+            // Reduce this row. f can't be zero because of previous check
+            let f = self[(i, j)];
+            for j_p in j..N {
+                self[(i, j_p)] /= f;
+            }
+            // Reduce other rows
+            for i_p in 0..M {
+                if i_p != i {
+                    let f = self[(i_p, j)];
+                    for j_p in j..N {
+                        self[(i_p, j_p)] -= f * self[(i, j_p)];
+                    }
+                }
+            }
+            // Advance pivot
+            i += 1;
+            j += 1;
+        }
+        self
+    }
+
+    pub fn no_nonzero_rows(&self) -> bool {
+        !self
+            .0
+            .iter()
+            .any(|row| row.into_iter().all(|ele| approx_eq!(f32, 0.0, *ele)))
     }
 }
 
@@ -135,5 +195,45 @@ where
             }
             new.assume_init()
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn rref() {
+        let mat = Matrix([
+            [1.0, 3.0, 1.0, 9.0],
+            [1.0, 1.0, -1.0, 1.0],
+            [3.0, 11.0, 5.0, 35.0],
+        ]);
+
+        let rref = Matrix([
+            [1.0, 0.0, -2.0, -3.0],
+            [0.0, 1.0, 1.0, 4.0],
+            [0.0, 0.0, 0.0, 0.0],
+        ]);
+
+        assert_eq!(mat.rref(), rref);
+    }
+
+    #[test]
+    fn no_nonzero_rows() {
+        let mat = Matrix([
+            [1.0, 3.0, 1.0, 9.0],
+            [1.0, 1.0, -1.0, 1.0],
+            [3.0, 11.0, 5.0, 35.0],
+        ]);
+
+        let rref = Matrix([
+            [1.0, 0.0, -2.0, -3.0],
+            [0.0, 1.0, 1.0, 4.0],
+            [0.0, 0.0, 0.0, 0.0],
+        ]);
+
+        assert!(mat.no_nonzero_rows());
+        assert!(!rref.no_nonzero_rows());
     }
 }
